e7e997d45a830427721ae344362003dfe721ca4c
arirahikkala <arirahikkala@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2010-03-22 19:32:34 +0000

    Switch all code involved in tracking the use of funds over to the ledger. Fixes the issue of funds taxed/funds spent showing as 0 in the title and high score screens. Silently breaks save compat because I'm still not quite sure how to deal with save version tracking vs. release versions.
    git-svn-id: http://svn.code.sf.net/p/lcsgame/code/trunk@427 a218ab89-6f2a-4548-b3d1-e91a371d7644

--- src/title/highscore.cpp
+++ src/title/highscore.cpp
@@ -330,8 +330,8 @@ void savehighscore(char endtype) {
     ustat_dead += stat_dead;
     ustat_kills += stat_kills;
     ustat_kidnappings += stat_kidnappings;
-    ustat_funds += stat_funds;
-    ustat_spent += stat_spent;
+    ustat_funds += ledger.total_income;
+    ustat_spent += ledger.total_expense;
     ustat_buys += stat_buys;
     ustat_burns += stat_burns;
 
@@ -341,7 +341,7 @@ void savehighscore(char endtype) {
     for(int s = 0; s < SCORENUM; s++) {
         if((endtype == END_WON && score[s].endtype == END_WON &&
                 year == score[s].year && month == score[s].month &&
-                stat_spent + stat_funds > score[s].stat_spent + score[s].stat_funds) ||
+                ledger.total_expense + ledger.total_expense > score[s].stat_spent + score[s].stat_funds) ||
 
                 (endtype == END_WON && score[s].endtype == END_WON
                  && (year < score[s].year ||
@@ -350,7 +350,7 @@ void savehighscore(char endtype) {
                 (endtype == END_WON && score[s].endtype != END_WON) ||
 
                 (endtype != END_WON && score[s].endtype != END_WON &&
-                 stat_spent + stat_funds > score[s].stat_spent + score[s].stat_funds) ||
+                 ledger.total_expense + ledger.total_income > score[s].stat_spent + score[s].stat_funds) ||
 
                 score[s].valid == 0) {
             for(int s2 = SCORENUM - 1; s2 >= s + 1; s2--)
@@ -363,8 +363,8 @@ void savehighscore(char endtype) {
             score[s].stat_dead = stat_dead;
             score[s].stat_kills = stat_kills;
             score[s].stat_kidnappings = stat_kidnappings;
-            score[s].stat_funds = stat_funds;
-            score[s].stat_spent = stat_spent;
+            score[s].stat_funds = ledger.total_income;
+            score[s].stat_spent = ledger.total_expense;
             score[s].stat_buys = stat_buys;
             score[s].stat_burns = stat_burns;
             score[s].valid = 1;
