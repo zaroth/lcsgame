31a06fcfdab4e4c6be177df28e5c2cc64d544d5b
blomkvist <blomkvist@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2010-11-14 18:05:51 +0000

    modified   art/weapons.xml Corrected daisho's name tags. modified   src/daily/shopsnstuff.cpp Made right location show for shops. modified   src/sitemode/shop.cpp Added a couple missing spaces.
    git-svn-id: http://svn.code.sf.net/p/lcsgame/code/trunk@466 a218ab89-6f2a-4548-b3d1-e91a371d7644

--- src/sitemode/shop.cpp
+++ src/sitemode/shop.cpp
@@ -547,9 +547,9 @@ int Shop::fenceselect(squadst &customers) const {
 
             if (location[customers.squad[0]->base]->loot[l]->get_number() > 1) {
                 if(selected[l] > 0)
-                    itemstr += tostring(selected[l]) + "/";
+                    itemstr += " " + tostring(selected[l]) + "/";
                 else
-                    itemstr += "x";
+                    itemstr += " x";
 
                 itemstr += tostring(location[customers.squad[0]->base]->loot[l]->get_number());
             }
