d9e0179ab13de289aced124f61cbb7ca3b050ae9
blomkvist <blomkvist@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2012-08-31 18:56:56 +0000

    game.cpp - Made value assignment of oldMapMode correspond to new return values of readConfigFile. talk.cpp - Correct name will be displayed when talking to dogs. siege.cpp - (At least some) Sieges should now end when they are over. Also removed a superfluous line.
    git-svn-id: http://svn.code.sf.net/p/lcsgame/code/trunk@593 a218ab89-6f2a-4548-b3d1-e91a371d7644

--- src/game.cpp
+++ src/game.cpp
@@ -298,9 +298,9 @@ int main(int argc, char *argv[]) {
     //refresh();
     //getch();
 
-    oldMapMode = readConfigFile("sitemaps.txt"); // load site map data
+    oldMapMode = !readConfigFile("sitemaps.txt"); // load site map data
 
-    if (oldMapMode == -1) {
+    if (oldMapMode == true) {
         addstr("Failed to load sitemaps.txt! Reverting to old map mode.");
         refresh();
         getch();
