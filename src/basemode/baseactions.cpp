66c3438800d6e976a0915424fd6b10e350b0126a
blomkvist <blomkvist@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2013-06-25 23:53:58 +0000

    baseactions.cpp - Allowed the stop evil screen to back up in one city mode too.
    world.cpp - Added cable news station to one city mode.
    
    
    git-svn-id: http://svn.code.sf.net/p/lcsgame/code/trunk@689 a218ab89-6f2a-4548-b3d1-e91a371d7644

--- src/basemode/baseactions.cpp
+++ src/basemode/baseactions.cpp
@@ -573,7 +573,7 @@ void stopevil(void) {
         }*/
 
         if(c == 10 || c == ESC) {
-            if(multipleCityMode && loc != -1 && (location[loc]->city != location[loc]->type || location[loc]->city != squad_location->city))
+            if(loc != -1 && (location[loc]->city != location[loc]->type || location[loc]->city != squad_location->city))
                 loc = location[loc]->parent;
             else {
                 activesquad->activity.type = ACTIVITY_NONE; // Clear squad activity
