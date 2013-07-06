2dfc08608b9a3adcf3f5b5bc4257fd447e630b2b
jonathansfox <jonathansfox@a218ab89-6f2a-4548-b3d1-e91a371d7644>
2013-07-06 00:05:55 +0000

    - President now appears as "President [Last Name]" instead of just "[Full Name]" - President now has proper name initialized and shouldn't appear as wrong/blank/garbage name when recruited or kidnapped - Fixed crash bug when disbanding
    git-svn-id: http://svn.code.sf.net/p/lcsgame/code/trunk@692 a218ab89-6f2a-4548-b3d1-e91a371d7644

--- src/creature/creature.cpp
+++ src/creature/creature.cpp
@@ -1559,7 +1559,7 @@ bool Creature::talkreceptive() {
     case CREATURE_RETIREE:
     case CREATURE_HAIRSTYLIST:
     case CREATURE_CLERK:
-    case CREATURE_MUTANT: // What? -- LK
+    case CREATURE_MUTANT:
         return true;
     }
 
@@ -1575,15 +1575,20 @@ bool Creature::can_date(Creature &a) {
     if(animalgloss || a.animalgloss)
         return true;
 
+    // Prohibit anyone 10 or younger
     if(age < 11 || a.age < 11)
         return false;
 
     if(age < 16 || a.age < 16) {
+        // Allow 11-15 year olds only if the other partner is
+        // within 4 years age difference
         if(ABS(age - a.age) < 5)
             return true;
         else
             return false;
-    } else
+    }
+    // Allow anyone 16 or older
+    else
         return true;
 }
 
@@ -1618,7 +1623,19 @@ void UniqueCreatures::newPresident() {
     Pres_state = UNIQUECREATURE_ALIVE;
 
     //Turn into President (not just random pol)
-    strcpy(Pres_.name, execname[EXEC_PRESIDENT]);
+    int len = strlen(execname[EXEC_PRESIDENT]);
+    char *pres_last_name;
+
+    for(int i = 0; i < len; i++) {
+        if(execname[EXEC_PRESIDENT][i] == ' ') {
+            pres_last_name = execname[EXEC_PRESIDENT] + i + 1;
+            break;
+        }
+    }
+
+    strcpy(Pres_.name, "President ");
+    strcat(Pres_.name, pres_last_name);
+    strcpy(Pres_.propername, execname[EXEC_PRESIDENT]);
     Pres_.dontname = true;
 
     switch(exec[EXEC_PRESIDENT]) {
